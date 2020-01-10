59bce0c96bb65a761d14099e5692f2c52e82ee73
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2020-01-10 16:33:20 +0100

    Subo los 6 primeros ejercicios

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -16,6 +16,21 @@ public class Practica1 {
     /**
      * @param args the command line arguments
      */
+    /*Leer 5 números (con bucle por favor, no pongáis 5 scanner consecutivos) y
+    mostrarlos en el mismo orden introducido.*/
+    
+    public static void imprimirLista() {
+        int[] arrayNum = new int [5];
+        for (int i=0;i<5;i++){
+            System.out.println("Dame un número");
+            Scanner num = new Scanner(System.in);
+            int num1 = num.nextInt();
+            arrayNum[i]=num1;
+        }
+        for (int i=0;i<arrayNum.length;i++){
+            System.out.println(arrayNum[i]);
+        }
+    }
     /*Leer 5 números y mostrarlos en orden inverso al introducido.*/
     public static void numerosRevertidos(){
         int lista_num[] = new int [5];
@@ -33,7 +48,7 @@ public class Practica1 {
     /*Leer 5 números por teclado y a continuación realizar la media de los
     números positivos, la media de los negativos y contar el número de ceros.*/
     
-    /*Opción válida pero no óptima*/
+    /*Opción válida pero no óptima, demasiada complicación*/
     /*public static void calcularMedia(){
         //utilizo floats para que al calcular la media se contemplen
         float num_positivos[] = new float [5];
@@ -134,36 +149,33 @@ el último, el segundo, el penúltimo, el tercero, etc.*/
     }
 /*Leer por teclado dos tablas de 10 números enteros y mezclarlas en una
 tercera de la forma: el 1º de A, el 1º de B, el 2º de A, el 2º de B, etc.*/
-    public static void mostrarTablas(){
+    public static void combinarTablas(){
         int tablaA[] = new int [10];
         int tablaB[] = new int [10];
         int tablaC[] = new int [20];
         int aux = 0; // contador para cargar valores a la segunda array
-        int contador = 0; //auxiliar para tablaC
+        int contadorA = 0; //auxiliar para tablaC
+        int contadorB = 0; //auxiliar para tablaC
         Scanner num = new Scanner(System.in);
         for (int i=0; i<20; i++){
             System.out.println("Dame un numero:");
             int num1 = num.nextInt();
-            if (tablaA.length != 10){
+            if (tablaA[9] == 0){
                 tablaA[i]=num1;
             }
             else {
                 tablaB[aux]=num1;
-                aux=+1;//el operador bien puesto como += no lo coge
+                aux+=1;
             }
         }
         for (int i=1;i<21;i++){
-            if (contador <=10){
-                if (i%2==0){
-                    tablaC[i-1]=tablaA[contador];
-                    contador+=1; //pendiente probar
-                }
-                else{
-                    tablaC[i-1]=tablaB[i-1];
-                }
+            if (i%2!=0){
+                tablaC[i-1]=tablaA[contadorA];
+                contadorA+=1; //pendiente probar
             }
             else{
-                
+                tablaC[i-1]=tablaB[contadorB];
+                contadorB+=1;
             }
         }
         for (int i=0;i<20;i++){
@@ -175,9 +187,53 @@ tercera de la forma: el 1º de A, el 1º de B, el 2º de A, el 2º de B, etc.*/
 mezclarlos en una tercera de la forma: 3 de la tabla A, 3 de la B, otros 3 de A,
 otros 3 de la B, etc.*/
     
+    public static void combinarTablasTres(){
+        int tablaA[] = new int [12];
+        int tablaB[] = new int [12];
+        int tablaC[] = new int [24];
+        int aux = 0;
+        int posA = 0;//variable para controlar los num que se van cargando en C
+        int posB = 0;
+        int contadorA = 0;//var para controlar cuantos num se han cargado de a
+        int contadorB = 0;
+        Scanner num = new Scanner(System.in);
+        for (int i=0; i<24; i++){
+            System.out.println("Dame un numero:");
+            int num1 = num.nextInt();
+            if (tablaA[11] == 0){
+                tablaA[i]=num1;
+            }
+            else {
+                tablaB[aux]=num1;
+                aux+=1;
+            }
+        }
+        for (int i=0;i<24;i++){
+            if (contadorA < 3){
+                tablaC[i]=tablaA[posA];
+                contadorA+=1;
+                posA+=1;
+                if (contadorA==3){
+                    contadorB=0;
+                }
+            }
+            else if (contadorB < 3){
+                tablaC[i]=tablaB[posB];
+                contadorB+=1;
+                posB+=1;
+                if (contadorB==3){
+                    contadorA=0;
+                }
+            }
+        }
+        
+        for (int i=0;i<24;i++){
+            System.out.println(tablaC[i]);
+        }
+    }
     
     public static void main(String[] args) {
         // TODO code application logic here
-        mostrarTablas();
+        
     }
 }
